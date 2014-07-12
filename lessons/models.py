e49c1f89a97e97a69edb0b43fbbae72c452de290
Noah Spahn <noah.de@gmail.com>
2014-07-12 12:08:30 -0700

    housekeeping

--- lessons/models.py
+++ lessons/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-# Create your models here.
-
 class Lesson(models.Model):
     title = models.CharField(max_length=150)
     text = models.TextField(default='')
--- lessons/views.py
+++ lessons/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from lessons.models import Lesson
 
-# Create your views here.
 def home_page(request):
     lessons = Lesson.objects.all()
     return render(request, 'home.html', {'lesson':lessons})
