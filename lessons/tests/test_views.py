a7d655a98cbb09e9e6820031ac2aaab83f177b09
Noah Spahn <noah.de@gmail.com>
2014-07-24 17:08:03 -0700

    more testing

--- lessons/tests/test_views.py
+++ lessons/tests/test_views.py
@@ -12,8 +12,17 @@ from unittest import skipIf
 class HomePageTest(TestCase):
     maxDiff = None
 
-    def test_home_page_returns_correct_html(self):
+    def test_home_page_uses_correct_template(self):
         request = HttpRequest()
         response = home_page(request)
         expected_html = render_to_string('home.html')
         self.assertMultiLineEqual(response.content.decode(), expected_html)
+
+    @skipIf(True,"not implemented")
+    def test_home_page_gets_recent_lessons_first(self):
+        # grab the time from the first lesson
+
+        # grab the time from the second lesson
+
+        # assert that 2nd is older
+        pass
