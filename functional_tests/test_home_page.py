db99ff63d9ebbc7554bda9411156e8c7011a7979
Noah Spahn <noah.de@gmail.com>
2014-07-30 13:06:27 -0700

    cleanup

--- functional_tests/test_home_page.py
+++ functional_tests/test_home_page.py
@@ -15,7 +15,6 @@ class HomePageTest(FunctionalTest):
         self.browser.get(self.server_url)
         self.browser.implicitly_wait(3)
 
-
         # taking a look at the title, the user confirms that this project was
         # written in a 'real' framework
         self.assertIn('feralo', self.browser.title)
@@ -28,14 +27,6 @@ class HomePageTest(FunctionalTest):
 
         # and publication date
         pub_date = self.browser.find_element_by_css_selector(".blog-post-date").text
-
-        # test that this is a date 
-
-        # the title of most recent lesson posted first
-
-        # the next older lesson is listed after that
-
-        #self.fail('finish testing')
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
