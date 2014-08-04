b242f36718b05a298d8d1254b632d76c673a9b92
Noah Spahn <noah.de@gmail.com>
2014-08-04 15:19:25 -0700

    adjusting delta (again) for TravisCI

--- lessons/tests/test_models.py
+++ lessons/tests/test_models.py
@@ -60,7 +60,7 @@ class LessonModelsTest(TestCase):
         # compare created time to modified time
         created_ms = lesson.created.microsecond
         modified_ms = lesson.modified.microsecond
-        self.assertAlmostEqual(created_ms,modified_ms, delta=10)
+        self.assertAlmostEqual(created_ms,modified_ms, delta=25)
         sleep(1)
 
         # modify lesson
@@ -68,6 +68,6 @@ class LessonModelsTest(TestCase):
         lesson.save()
 
         # assert that the modify times don't match
-        self.assertNotAlmostEqual(created_ms, lesson.modified.microsecond, delta=10)
+        self.assertNotAlmostEqual(created_ms, lesson.modified.microsecond, delta=25)
         self.assertNotEqual(modified_ms, lesson.modified.microsecond)
         
