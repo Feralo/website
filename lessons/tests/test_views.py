ded14e5301694e7ea4b995f19adaebd43342e0ab
Noah Spahn <noah.de@gmail.com>
2014-08-04 22:07:35 -0700

    markdown template tag working... needs more functional testing

--- lessons/tests/test_views.py
+++ lessons/tests/test_views.py
@@ -3,11 +3,13 @@ from django.core.urlresolvers import resolve
 from django.http import HttpRequest
 from django.test import TestCase
 from django.utils.html import escape
+from django.utils import timezone
 
 from lessons.models import Lesson
 from lessons.views import home_page
 
 from unittest import skipIf
+import markdown
 
 class HomePageTest(TestCase):
     maxDiff = None
@@ -26,3 +28,18 @@ class HomePageTest(TestCase):
         oldest_post_index = response.content.index(b'Mastoklet')
         newer_post_index = response.content.index(b'Tendamosi')
         self.assertTrue(newer_post_index < oldest_post_index)
+
+    # @skipIf(True,"not yet implemented")
+    def test_view_displays_markdown(self):
+        lesson = Lesson()
+        lesson.title = 'Marcolis'
+        lesson.text = 'Divinostrum caliromis [salvitorium](http://127.0.0.1:8000/)'
+        lesson.created = timezone.now()
+        lesson.save()
+        
+        request = HttpRequest()
+        response = home_page(request)
+        print(markdown.markdown(lesson.text))
+        self.assertTrue(markdown.markdown(lesson.text) in str(response.content))
+        
+        
