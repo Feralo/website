4e6d24e837351bf125363fc014ea5a437472b347
Noah Spahn <noah.de@gmail.com>
2014-08-08 17:40:14 -0700

    clean up migrations

--- lessons/migrations/0001_initial.py
+++ lessons/migrations/0001_initial.py
@@ -1,5 +1,6 @@
 # encoding: utf8
 from django.db import models, migrations
+import datetime
 
 
 class Migration(migrations.Migration):
@@ -11,11 +12,15 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Lesson',
             fields=[
-                ('id', models.AutoField(auto_created=True, serialize=False, verbose_name='ID', primary_key=True)),
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
                 ('title', models.CharField(max_length=150)),
                 ('text', models.TextField(default='')),
+                ('modified', models.DateTimeField(default=datetime.datetime(2014, 8, 8, 17, 38, 59, 484457), verbose_name='modified')),
+                ('created', models.DateTimeField(default=datetime.datetime(2014, 8, 8, 17, 38, 59, 484500), verbose_name='created')),
+                ('published', models.BooleanField(default=False)),
             ],
             options={
+                'ordering': ['-created'],
             },
             bases=(models.Model,),
         ),
