a63ac343816ac02f15e31e0e97595b16ecf2ab6d
Noah Spahn <noah.de@gmail.com>
2014-12-21 20:13:29 -0800

    remove comments/clutter

--- feralo/urls.py
+++ feralo/urls.py
@@ -12,11 +12,9 @@ urlpatterns = patterns('',
      context_object_name = "lesson", 
      paginate_by=5,
      )),
-        # Individual posts
-    #url(r'^(?P<pub_date__year>\d{4})/(?P<pub_date__month>\d{1,2})/(?P<slug>[a-zA-Z0-9_]+)/?$', DetailView.as_view(
-    #url(r'^(?P<created__year>\d{4})/(?P<created__month>\d{1,2})/(?P<created__day>\d{1,2})/(?P<pk>\d+)/?$', DetailView.as_view(
+    
+    # Individual posts
     url(r'^(?P<created__year>\d{4})/(?P<created__month>\d{1,2})/(?P<created__day>\d{1,2})/(?P<slug>[a-zA-Z0-9-]+)/?$', DetailView.as_view(
-    #url(r'^(?P<pkid>\d{1})/?$', DetailView.as_view(
         model=Lesson,
         template_name = 'lesson.html',
         context_object_name = "lesson",
