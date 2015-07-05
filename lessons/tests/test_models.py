f5992ecd08ec60a1fe78ad63937a7038e0197b81
Noah Spahn <noah.de@gmail.com>
2015-07-04 22:22:50 -0700

    more accurate test of timestamp difference

--- lessons/tests/test_models.py
+++ lessons/tests/test_models.py
@@ -61,7 +61,7 @@ class LessonModelsTest(TestCase):
         # compare created time to modified time
         created_ms = lesson.created.microsecond
         modified_ms = lesson.modified.microsecond
-        self.assertAlmostEqual(created_ms,modified_ms, delta=95)
+        self.assertTrue(created_ms < modified_ms)
         sleep(1)
 
         # modify lesson
